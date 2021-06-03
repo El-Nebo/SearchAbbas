bf5007d2b01f01e0b9bf80f4cfce065911a1b739
Mohamed Salama <62220722+Mohammed-Salama@users.noreply.github.com>
2021-06-03 03:35:29 +0200

    Update CrawlerThread.java

--- crawler/CrawlerThread.java
+++ crawler/CrawlerThread.java
@@ -1,51 +1,98 @@
 package crawler;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.Statement;
-import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.HashSet;
+import java.util.LinkedList;
+
 import java.sql.*;
-public class CrawlerMain {
-    static int ThreadsNum;
-    public static void main(String[] args)throws Exception
-    {
-        //ConnectToMySql ();
-        System.out.println("Start CrawlerThread");
-        //Initialize Crawler Parameters
-        CrawlerThread.init();
 
-        //get ThreadsNumber
-        System.out.println("Please Enter Threads Number :");
-        Scanner scanner=new Scanner(System.in);
-        ThreadsNum=scanner.nextInt();
-        scanner.close();
+public class CrawlerThread extends Thread {
+    public static HashSet<String> links;
+    public final int MAXVISITS = 200;
+    public static LinkedList<String> q;
+    public static Connection con;
+    public static void init() {
+       ConnectToMySql();
+        links = new HashSet<String>();
+        q = new LinkedList<String>();
+    }
 
+    @Override
+    public void run() {
 
-        // Add Some Seeds
-        //CrawlerThread.q.add("https://www.netflix.com/eg-en/browse/genre/34399");
-        CrawlerThread.q.add("https://en.wikipedia.org/wiki/");
-       // CrawlerThread.q.add("https://www.geeksforgeeks.org/");
-      //  CrawlerThread.q.add("https://www.w3schools.com/");
+        try {
 
-        //Create Threads
-        CrawlerThread [] threads=new CrawlerThread[ThreadsNum];
-        for(int i = 0; i<ThreadsNum ;i++)
-        {
+            while (links.size() < MAXVISITS) {
 
-           threads[i]=new CrawlerThread();
-           threads[i].start();
+                synchronized (q) {
+                    while (q.isEmpty())
+                        q.wait();
+                }
+                String URL = q.poll();
+
+                // 2. Fetch the HTML code
+                Document document = Jsoup.connect(URL).get();
+                // 3. Parse the HTML to extract links to other URLs
+                Elements linksOnPage = document.select("a[href]");
+
+                for (Element page : linksOnPage) {
+                    String newUrl = page.attr("abs:href");
+                    boolean createFile = false;
+                    int fileNum = 0;
+
+                    synchronized (q) {
+                        if (!links.contains(newUrl) && links.size() < MAXVISITS) {
+                            links.add(newUrl);
+                            q.add(newUrl);
+                            //String query = "INSERT INTO urls " + "value ( ' " +newUrl+ " ' );" ;
+                            Statement stmt=con.createStatement();
+
+                            // stmt.executeUpdate(query);
+                            createFile = true;
+                            fileNum = links.size();
+
+                            q.notify(); // wake up only one thread as we only added one new link
+                        }
+                    }
+
+                    if (createFile) {
+                        // Create the file
+                        File myfile = new File("Htmls/" + fileNum + ".html");
+                        myfile.createNewFile();
+
+                        // Write to the File
+                        FileWriter fileWriter = new FileWriter(myfile);
+                        try {
+                            fileWriter.write(Jsoup.connect(newUrl).get().html());
+                        } catch (Exception e) { // invalid url lead to empty file
+
+                        }
+                        fileWriter.close();
+
+                    }
+
+                }
+
+            }
+            // public void getPageLinks(String URL,Queue<String> q) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        for(int i = 0; i<ThreadsNum ;i++)
-        {
-            threads[i].join();
-            System.out.println("Thread number "+i +" Finished");
-        }
 
-        //finised crawling
-        int count=CrawlerThread.links.size();
-        for (String url : CrawlerThread.links) {
-            System.out.println(url);
-        }
-        System.out.println("Number of files:" +count);
+
+    }
+    public static void ConnectToMySql(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/search_engine?characterEncoding=latin1&useConfigs=maxPerformance","root","01149873532");
+            Statement stmt=con.createStatement();
+        }catch(Exception e){ System.out.println(e);}
     }
 }
