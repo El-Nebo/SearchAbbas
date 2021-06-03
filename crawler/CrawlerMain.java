620aecfc4b2a97fba946c82749d39677e0c58718
Mohamed Salama <62220722+Mohammed-Salama@users.noreply.github.com>
2021-06-03 03:36:00 +0200

    Update CrawlerMain.java

--- crawler/CrawlerMain.java
+++ crawler/CrawlerMain.java
@@ -1,33 +1,39 @@
 package crawler;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
 import java.util.Scanner;
-
+import java.sql.*;
 public class CrawlerMain {
     static int ThreadsNum;
     public static void main(String[] args)throws Exception
     {
+        //ConnectToMySql ();
         System.out.println("Start CrawlerThread");
         //Initialize Crawler Parameters
         CrawlerThread.init();
 
-        //get ThreadsNumber 
+        //get ThreadsNumber
         System.out.println("Please Enter Threads Number :");
         Scanner scanner=new Scanner(System.in);
         ThreadsNum=scanner.nextInt();
         scanner.close();
 
+
         // Add Some Seeds
         //CrawlerThread.q.add("https://www.netflix.com/eg-en/browse/genre/34399");
         CrawlerThread.q.add("https://en.wikipedia.org/wiki/");
-        CrawlerThread.q.add("https://www.geeksforgeeks.org/");
-        CrawlerThread.q.add("https://www.w3schools.com/");
+       // CrawlerThread.q.add("https://www.geeksforgeeks.org/");
+      //  CrawlerThread.q.add("https://www.w3schools.com/");
 
-        //Create Threads  
+        //Create Threads
         CrawlerThread [] threads=new CrawlerThread[ThreadsNum];
         for(int i = 0; i<ThreadsNum ;i++)
         {
 
-            threads[i]=new CrawlerThread();
-            threads[i].start();
+           threads[i]=new CrawlerThread();
+           threads[i].start();
         }
         for(int i = 0; i<ThreadsNum ;i++)
         {
